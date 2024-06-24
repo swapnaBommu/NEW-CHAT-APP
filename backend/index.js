638993a5fbe3b1adcf76b1619f66f2460ff934c6
swapnaBommu <swapnabommu8143@gmail.com>
2024-06-24 22:35:53 +0530

    send message endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
-// import messageRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/message.route.js";
 // import userRoutes from "./routes/user.route.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -17,7 +17,7 @@ app.use(express.json()); // to parse the incoming requests with JSON payloads (f
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
+app.use("/api/messages", messageRoutes);
 // app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
