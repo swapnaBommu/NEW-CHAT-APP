216aa9837b10d14907a6d95673c7223d1fbd17b7
swapnaBommu <swapnabommu8143@gmail.com>
2024-06-25 16:52:40 +0530

    fixed untracked files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+// import SignUp from "./pages/signup/Signup";
+
+function App() {
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      {/* <Login /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
