62293dfd9b3926954dd4f7dd1262a706d2b73e60
swapnaBommu <swapnabommu8143@gmail.com>
2024-06-26 17:09:03 +0530

    login functionality

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
   const signup = async ({
     fullName,
     username,
