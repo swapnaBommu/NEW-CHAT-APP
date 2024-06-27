5d434f7152e8791f81e6b64dfa591d83e2411bc5
swapnaBommu <swapnabommu8143@gmail.com>
2024-06-27 06:58:32 +0530

    send message functinality

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -4,24 +4,7 @@ const Messages = () => {
   return (
     <div className="px-4 flex-1 overflow-auto">
       <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+
       <Message />
     </div>
   );
